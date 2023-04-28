9996779b4190d92ffe5db551905e0c95f5faf25a
wfrank90 <wolfangf90@gmail.com>
2023-04-28 17:47:05 -0500

    ejercicio06

---
+++ rick_and_morty/src/components/Cards.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './Card';
+
+export default function Cards(props) {
+   const {characters} = props;
+
+   return ( <div>
+      {characters.map((char)=> {
+         return(
+         <Card
+         key={char.id}
+         name={char.name}
+         status= {char.status}
+         species={char.species}
+         gender={char.gender}
+         origin={char.origin.name}
+         image={char.image}
+         onClose = {() => window.alert ("Emular cierre de terjeta")}
+         />
+      );
+      })}
+   </div>
+   );
+}
